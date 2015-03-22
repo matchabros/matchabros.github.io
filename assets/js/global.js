a40ee0f74be57fd690b3133a2946eb7466116c12
matchabros <chabros.matt@gmail.com>
2015-03-22 23:05:34 +0000

    Another commit

---
+++ assets/js/global.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	var aboutContact = $('.about-contact');
+	var btn = $('#about-nav');
+    //Hide the About me/Contact section
+	aboutContact.hide();
+
+    $(function () {
+    	btn.on('click', function () {
+	        aboutContact.fadeIn(300);
+            $('.about-me p:first').addClass('fadeIn-animation');//Add the animaition class
+            $('.about-me h1:first').addClass('underline-animation');//Add the animaition class
+            $('.about-me a').addClass('social-animation');//Add the animation class
+        });
+    });
+    $(function () {
+    	aboutContact.on('click', function (){
+    		aboutContact.fadeOut(300);
+    	});
+    });
+});
+
