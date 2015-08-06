d621d87f5e77bffaec6e193739ea57013a7d576a
Mateusz Chabros <chabros.matt@gmail.com>
2015-08-06 11:44:58 +0100

    new web/fixed .gitignore

--- README.md
+++
@@ -1,5 +0,0 @@
-<h1>The new website </h1>
-
-<a href="http://matchabros.com">www.matchabros.com</a>
-
-<p>This will be my playground for the time being. Please do enjoy the stay if you decide to have a look </p>
---
+++ gulpfile.babel.js
@@ -0,0 +1,54 @@
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+
+const $ = gulpLoadPlugins();
+const bs = browserSync.create();
+
+const scssPaths = {
+  scssAssets: './assets/scss'
+};
+
+const distPaths = {
+  distCss: './public/css',
+  distJs: './public/js'
+};
+
+function errorAlert(err) {
+  $.notify.onError({
+    title: 'Gulp Error',
+    message: 'An error occured, check your terminal',
+    sound: 'Basso'
+  })(err);
+  gutil.log(gutil.colors.red(err.toString()));
+  this.emit('end');
+}
+
+gulp.task('scss->css', () => {
+  return gulp.src(scssPaths.scssAssets + '/main.scss')
+  .pipe($.plumber({errorHandler: errorAlert}))
+  .pipe($.sass({
+    includePaths: [
+      scssPaths.scssAssets]}))
+  .pipe($.autoprefixer({
+    browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Android >= 4']
+  }))
+	.pipe($.minifyCss())
+	.pipe($.rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest(distPaths.distCss))
+  .pipe($.notify({
+    title: 'Stylesheets recompiled',
+    message: '<%= file.relative %>'
+  }));
+});
+
+gulp.task('default', () => {
+  bs.init({
+    browser: 'google chrome',
+    server: './public/'
+  });
+  gulp.watch(scssPaths.scssAssets + '/**/*.scss', ['scss->css', bs.reload]);
+});
